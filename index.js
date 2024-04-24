7163b5258674bce6b7599ec00280985359f17e00
Miraj Patel <mirajpatel@Mirajs-MacBook-Air.local>
2024-04-24 16:31:34 -0500

    config delpoy

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 morgan.token('data', (req, res) => {
     return JSON.stringify(req.body)
@@ -88,7 +90,7 @@ app.post('/api/persons/', (request, response) => {
     }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
